0fdb083d191621db3481ea08c519740904ef1314
ramsalo1981 <rami.salloum@hotmail.com>
2023-10-25 16:37:09 +0200

    first commit

---
+++ CAProcessAndThread/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace CAProcessAndThread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Process Id: {Process.GetCurrentProcess().Id}");
+            Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"Processor Id: {Thread.GetCurrentProcessorId()}");
+
+            Console.ReadKey();
+        }
+    }
+}
